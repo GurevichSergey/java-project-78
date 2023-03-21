c70f605e2ee55f86a08eb7b861155858b8045f2d
sergeygurevich <spakro1802@gmail.com>
2023-03-22 01:54:15 +1000

    refactoring NumberSchema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,17 +1,15 @@
 package hexlet.code.schemas;
 
 public class NumberSchema extends BaseSchema {
-    public NumberSchema required() {
-        addConditions(condition -> condition instanceof Number && condition != null);
-        return this;
+    public void required() {
+        addConditions(condition -> condition instanceof Number);
     }
     public NumberSchema positive() {
         addConditions(condition -> condition instanceof Integer && (Integer) condition > 0
                 || condition == null);
         return this;
     }
-    public NumberSchema range(int begin, int end) {
+    public void range(int begin, int end) {
         addConditions(condition -> (Integer) condition >= begin && (Integer) condition <= end);
-        return this;
     }
 }
